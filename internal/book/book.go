984f4cc706e06e03f39d58936b7ba4eaa98644c8
Chamika Jayasinghe <jayasinghech.21@uom.lk>
2026-03-17 10:59:15 +0530

    feat: defined book and the constructor

--- internal/book/book.go
+++ internal/book/book.go
@@ -1,3 +1,30 @@
 // maintains per symbol order book state (btc / eth ...) + applies incremental delta updates and produces snapshots for downstream clients
 
 package book
+
+import (
+	"sync"
+	"time"
+
+	"market-gw.com/internal/domain"
+)
+
+type Book struct {
+	mu     sync.RWMutex // ! to avoid race conditions between pipeline writes & ws server reads
+	symbol domain.Symbol
+
+	bids map[string]string
+	asks map[string]string
+
+	lastUpdated time.Time
+}
+
+func NewBook(symbol domain.Symbol) *Book {
+
+	return &Book{
+		symbol: symbol,
+		// maps:- instant lookups for delta updates
+		bids: make(map[string]string), // price:quantity
+		asks: make(map[string]string), // price: quantity
+	}
+}
