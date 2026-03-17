972daf078f08a5ad464703198d221a1b9483e8f1
Chamika Jayasinghe <jayasinghech.21@uom.lk>
2026-03-17 11:24:04 +0530

    feat: started implementing apply function in book - implemented update procedure for updatetype = snapshot

--- internal/book/book.go
+++ internal/book/book.go
@@ -10,7 +10,7 @@ import (
 )
 
 type Book struct {
-	mu     sync.RWMutex // ! to avoid race conditions between pipeline writes & ws server reads
+	mu     sync.RWMutex // to avoid race conditions between pipeline writes & ws server reads - multiple go routines to read using RLock() + pipeline when writing Lock()
 	symbol domain.Symbol
 
 	bids map[string]string
@@ -27,4 +27,30 @@ func NewBook(symbol domain.Symbol) *Book {
 		bids: make(map[string]string), // price:quantity
 		asks: make(map[string]string), // price: quantity
 	}
+}
+
+func (b *Book) Apply(u domain.Update) {
+	b.mu.Lock()
+	defer b.mu.Unlock()
+
+	switch u.Type {
+
+	case domain.UpdateTypeSnapshot:
+		b.bids = make(map[string]string, len(u.Bids))
+		b.asks = make(map[string]string, len(u.Asks))
+
+		for _, level := range u.Bids {
+			b.bids[level.Price] = level.Quantity
+		}
+
+		for _, level := range u.Asks {
+			b.asks[level.Price] = level.Quantity
+		}
+
+	case domain.UpdateTypeDelta:
+		// TODO
+	}
+
+	b.lastUpdated = u.ReceivedAt
+
 }
