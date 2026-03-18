99ba6349172f89c4ceca287e12960df8886fbb30
Chamika Jayasinghe <chamika.21@cse.mrt.ac.lk>
2026-03-18 17:22:18 +0530

    feat: added binance wire types and symbol conversion

--- internal/adapter/binance/binance.go
+++ internal/adapter/binance/binance.go
@@ -3,6 +3,7 @@ package binance
 import (
 	"context"
 	"fmt"
+	"strings"
 
 	"market-gw.com/internal/domain"
 )
@@ -50,5 +51,55 @@ func (a *Adapter) Run(ctx context.Context) (<-chan domain.Update, error) {
 func (a *Adapter) runSymbol(ctx context.Context, sym domain.Symbol) (<-chan domain.Update, error) {
 	// TODO:
 
+	// ! TEMP USAGE TO PASS THE LINT
+	_ = canonicalToBinanceNative(sym)
+
+	var r restDepthResponse
+	var w wsDepthEvent
+
+	_ = binancePairsToLevels(r.Bids)
+	_ = w.Symbol
+
 	return nil, nil
+}
+
+// GET https://api.binance.com/api/v3/depth
+type restDepthResponse struct {
+	LastUpdateID int         `json:"lastUpdateId"`
+	Bids         [][2]string `json:"bids"`
+	Asks         [][2]string `json:"asks"`
+}
+
+// wss://stream.binance.com:9443/ws/btcusdt@depth
+type wsDepthEvent struct {
+	EventType     string      `json:"e"`
+	EventTime     int64       `json:"E"`
+	Symbol        string      `json:"s"`
+	FirstUpdateID int64       `json:"U"`
+	FinalUpdateID int64       `json:"u"`
+	Bids          [][2]string `json:"b"`
+	Asks          [][2]string `json:"a"`
+}
+
+// "BTC-USD" --> "BTCUSDT" || "ETH-USD" --> "ETHUSDT"
+func canonicalToBinanceNative(sym domain.Symbol) string {
+	s := strings.ReplaceAll(string(sym), "-", "")
+	if strings.HasSuffix(s, "USD") {
+		s = s[:len(s)-3] + "USDT"
+	}
+	return strings.ToUpper(s)
+}
+
+func binancePairsToLevels(pairs [][2]string) []domain.Level {
+
+	if len(pairs) == 0 {
+		return nil
+	}
+
+	levels := make([]domain.Level, len(pairs))
+	for i, p := range pairs {
+		levels[i] = domain.Level{Price: p[0], Quantity: p[1]}
+	}
+	return levels
+
 }
